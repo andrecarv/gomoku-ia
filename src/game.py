bf792ea7ed4fc6082bffab2924bfcf7942be0ee6
diegohkd <diegohkd@hotmail.com>
2016-09-21 12:51:07 -0300

    added new game dialog

--- src/game.py
+++ src/game.py
@@ -1,12 +1,14 @@
-'''
+﻿'''
 Created on Sep 5, 2016
 
 @author: andre
 '''
 from control.user_input import UserInput
 from gui.main_window import MainWindow
+from gui.dialog_new_game import Ui_DialogNewGame
 from model.board import Board
 from model.piece import Piece
+from PyQt5.QtWidgets import QDialog
 
 PLAYER_COLOR = {1: 'green', -1:'red'}
 
@@ -21,12 +23,21 @@ class Game(object):
         '''
         self.board = None
         main_window = MainWindow()
+
+        self.dialog_new_game = QDialog()
+        self.ui = Ui_DialogNewGame()
+        self.ui.setupUi(self.dialog_new_game)
+
         self.user_input = UserInput(main_window, self)
         self.new_game()
         self.over = False
         main_window.show()
         
     def new_game(self):
+        self.dialog_new_game.exec()
+        print(self.ui.is_player2_human)
+        print(self.ui.difficulty)
+
         self.board = Board()
         self.over = False
         self.active_player = 1
@@ -38,7 +49,7 @@ class Game(object):
         return False
     
     def check_game_end(self):
-        result = self.board.verify_game_over2()
+        result = self.board.verify_game_over()
         if result == 'end':
             self.over = True
         return result
